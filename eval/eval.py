d461c420a4c57b409311a08e2e7cab917d1ba3f6
Wang helin <hlwang215@outlook.com>
2025-09-21 13:30:25 +0800

    feat: eval workflow

--- eval/eval.py
+++ eval/eval.py
@@ -2,11 +2,28 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'app'))
-print(sys.path)
 from agents.graph import retriever_test_workflow_app
+import argparse
+import json
 
 if __name__ == "__main__":
-    result_state = retriever_test_workflow_app.invoke({"query": "什么时候公布学业水平考试的成绩？"})
-    # print the retrieved docs
-    print(result_state["docs"])
-    print(result_state["reranked_docs"])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--qapair_filepath", help="QAPair Filepath", default="../data/eval/simpleQA_v0.json")
+    args = parser.parse_args()
+    qapair_filepath = args.qapair_filepath
+    
+    # Load qapairs
+    with open(qapair_filepath, "r", encoding="utf-8") as f:
+        qapairs = json.load(f)
+    
+    for qapair in qapairs:
+        print(qapair)
+        question = qapair["question"]
+        result_state = retriever_test_workflow_app.invoke({"query": question})
+        # print the retrieved docs
+        print(result_state)
+        break
+    # result_state = retriever_test_workflow_app.invoke({"query": qapairs[0]["question"]})
+    # # print the retrieved docs
+    # print(result_state["docs"])
+    # print(result_state["reranked_docs"])
